ed3c3378711907a9c7900554d85d20500710c57d
Flonger <xue@flonger.com>
2017-02-23 16:50:07 +0800

    Initial Commit

---
+++ MVVMDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MVVMDemo
+//
+//  Created by 薛飞龙 on 2017/2/23.
+//  Copyright © 2017年 薛飞龙. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
